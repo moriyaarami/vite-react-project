7646003f14631118feef8830d5e0bb2cbd648239
moriya <moriyastudent@gmail.com>
2024-10-02 00:07:16 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <div className="app min-vh-100 d-flex flex-column gap-2" >
       <Header />
       <main className="flex-fill">
-
+        <h6>hii</h6>
         <Routes>
           <Route path="/home" element={<Home />} />
 
